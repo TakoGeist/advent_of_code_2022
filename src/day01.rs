95c6554479c3f3bf9e2073937d68796b0b3d7b07
Tako <d.silva.ramos12@gmail.com>
2022-12-01 23:52:28 +0000

    Completed day01

--- src/day01.rs
+++ src/day01.rs
@@ -1,7 +1,33 @@
+fn most_calories(input: &str) -> u32{
+    input.split("\r\n\r\n")
+        .map(|elf| elf.lines()
+        .map(|line| line.parse::<u32>().unwrap())
+        .sum())
+        .max()
+        .unwrap()
+}
+    
+fn three_most_calories(input: &str) -> u32{
+    let mut sums = 
+        input.split("\r\n\r\n")
+            .map(|elf| elf.lines()
+            .map(|line| line.parse::<u32>().unwrap())
+            .sum::<u32>())
+            .collect::<Vec<_>>();        
+    
+    sums.sort_unstable_by(|a,b| b.cmp(a));
+    
+    sums.iter()
+        .take(3)
+        .sum()    
+}
+
 pub fn part1(){
-    todo!()
+    let input = std::fs::read_to_string("input/day01.txt").unwrap();
+    println!("Part 1 -> {}", most_calories(&input))
 }
 
 pub fn part2(){
-    todo!()
+    let input = std::fs::read_to_string("input/day01.txt").unwrap();
+    println!("Part 2 -> {}", three_most_calories(&input))    
 } 
