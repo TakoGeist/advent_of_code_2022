9f169f39d79a203aa310af5f84cbcdd6e7584c1f
Tako <d.silva.ramos12@gmail.com>
2022-12-06 12:31:30 +0000

    Completed day06

--- src/day06.rs
+++ src/day06.rs
@@ -1,7 +1,42 @@
+
+fn start_of_packet(input: &str) -> usize{
+    let input = input.as_bytes();
+    'outer: for pos in 4..input.len(){
+        let mut visited = [false;26];
+        for ind in pos-4..pos{
+            let visit = &mut visited[(input[ind]-b'a') as usize]; 
+            if *visit{
+                continue 'outer;
+            }
+            *visit = true;
+        }
+        return pos;
+    }
+    0
+}
+
+fn message(input: &str) -> usize{
+    let input = input.as_bytes();
+    'outer: for pos in 14..input.len(){
+        let mut visited = [false;26];
+        for ind in pos-14..pos{
+            let visit = &mut visited[(input[ind]-b'a') as usize]; 
+            if *visit{
+                continue 'outer;
+            }
+            *visit = true;
+        }
+        return pos;
+    }
+    0
+}
+
 pub fn part1(){
-    todo!()
+    let input = std::fs::read_to_string("input/day06.txt").unwrap();
+    println!("Part 1 -> {}",start_of_packet(&input));
 }
 
 pub fn part2(){
-    todo!()
+    let input = std::fs::read_to_string("input/day06.txt").unwrap();
+    println!("Part 2 -> {}",message(&input));
 } 
